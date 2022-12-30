a6729bfd205138affefcd32be17d678f22293b7b
Will Holmes <willholmes95@gmail.com>
2022-12-29 23:43:39 -0500

    Add default publicPath to webpack output to fix issue with relative bundle linking (caused unstyled 404 page for subdirs in prod)

--- webpack.config.js
+++ webpack.config.js
@@ -13,6 +13,7 @@ module.exports = {
     filename: '[name][contenthash].js',
     clean: true,
     assetModuleFilename: '[name][ext]',
+    publicPath: '/',
   },
   devtool: 'source-map',
   devServer: {
